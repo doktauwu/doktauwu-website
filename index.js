58014ac136eedf149339b125d16a04a66478895b
(Moved) <98882733+doktauwu@users.noreply.github.com>
2022-02-04 00:23:59 +0900

    Several changes
    Added English support,
    added link icon alts

---
+++ index.js
@@ -0,0 +1,38 @@
+import localizationFile from "./localization.json" assert{type: "json"}
+
+let language;
+
+if (navigator.language === "ko") {
+    language = "ko"
+}
+else {
+    language = "en"
+}
+
+// Replace Language
+
+const replaceLanguage = () => {
+    if (language === "ko") {
+        language = "en"
+    }
+    else {
+        language = "ko"
+    }
+
+    const targetTexts = document.querySelectorAll(".lang-text");
+
+    targetTexts.forEach((element) => {
+        try {
+            const textcode = element.getAttribute("textcode");
+            element.innerHTML = localizationFile.textDictionary[localizationFile.shortcut[textcode]][language]
+        }
+        catch { console.error("Failed to read localizing content") }
+    })
+
+}
+
+
+
+// Language Button Event Listner
+
+document.querySelector(".gn-lang-btn").addEventListener("click", replaceLanguage)
